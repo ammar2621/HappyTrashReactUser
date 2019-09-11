b4766dff1498284a712c6e194cd5a4cc58de7b9b
ammar2621 <ammar@alterra.id>
2019-09-11 15:30:36 +0700

    copy dari master

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React, { Component } from "react";
-import { MDBBtn, MDBCol, MDBContainer, MDBRow } from "mdbreact";
-import "./index.css";
-import logo from "./logo.png";
+import React from "react";
+import {
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardImage,
+  MDBCardBody,
+  MDBBadge
+} from "mdbreact";
 
-class App extends Component {
-  render() {
-    return (
-      <MDBContainer>
-        <MDBRow center style={{ height: "100vh" }}>
-          <MDBCol middle="true" sm="8" className="text-center">
-            <img src={logo} alt="logo" style={{ width: "10rem" }} />
-            <h1>Welcome to Your MDB React App</h1>
-            <p className="mb-2">The application is configured and ready to import our components.</p>
-            <MDBBtn href="https://mdbootstrap.com/docs/react/" target="blank" color="light-blue"><strong>Check out our docs!</strong></MDBBtn>
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
-    );
-  }
-}
+const EcommercePage = () => {
+  return (
+    <MDBRow className="justify-content-center">
+      <MDBCol style={{ maxWidth: "480px" }}>
+        <div
+          style={{
+            height: "100vh",
+            backgroundColor: "#377C4E",
+            textAlign: "center"
+          }}
+        >
+          <br />
+          <h1 style={{ color: "#F0F0F0" }}>Sampah Online</h1>
+        </div>
+      </MDBCol>
+    </MDBRow>
+  );
+};
 
-export default App;
+export default EcommercePage;
